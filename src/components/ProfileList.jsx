8c66296bb45eb026234196c024df98ca17d9fa40
seoin <seoin@virnect.com>
2022-11-21 20:19:01 +0900

    refactor:make ProfileList component

---
+++ src/components/ProfileList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Profile from './components/Profile';
+
+export default function ProfileList() {
+  return (
+    <div>
+      <Profile isNew image='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80' name='jane' job='frontend developer'/>
+      <Profile image='https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80' name='julle' job='backend developer' />
+      <Profile name='dale' job='ios developer' image='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80'/>
+    </div>
+  );
+}
+
