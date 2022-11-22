39c5ae16501816a2bae673645340aac6dc2cae57
seoin <seoin@virnect.com>
2022-11-22 20:22:32 +0900

    feat:마우스 따라 가기 과제 수행

--- src/advanced/components/AppXY.jsx
+++ src/advanced/components/AppXY.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
+  const [x,setX] =useState(0)
+  const [y,setY] =useState(0)
+
+  const handleMouseMove=(event)=>{  
+    setX(event.clientX)
+    setY(event.clientY)
+  }
+
   return (
-    <div className='container'>
-      <div className='pointer' />
+    <div className='container' onMouseMove={handleMouseMove}>
+      <div className='pointer' style={{left:x+'px', top:y+'px'}}/>
     </div>
   );
 }
